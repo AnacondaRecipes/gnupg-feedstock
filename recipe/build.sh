88af8b10ade3bbbfdcfdce79d81f77bcb2b52956
Mohamed Sentissi <msentissi@anaconda.com>
2025-01-21 13:20:42 -0500

    clean up build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#export CPPFLAGS="-DIN_EXCL_UNLINK=0x04000000 $CPPFLAGS"  # [linux]
-#export LDFLAGS="-lrt $LDFLAGS"                           # [linux]
 ./configure --prefix=$PREFIX --enable-all-tests
 make
 make check
