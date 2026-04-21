37235262bc845a12a521cf6fe9f7cf1571859d9d
Andrii-Osipov <aosipov@anaconda.com>
2026-04-21 09:32:07 -0700

    gnupg 2.5.18

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-./configure --prefix=$PREFIX --enable-all-tests
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
+
+./configure \
+    --disable-doc \
+    --prefix=$PREFIX \
+    --with-npth-prefix=$PREFIX \
+    --with-libgpg-error-prefix=$PREFIX \
+    --with-libgcrypt-prefix=$PREFIX \
+    --with-libksba-prefix=$PREFIX \
+    --with-libassuan-prefix=$PREFIX \
+    --with-ntbtls-prefix=$PREFIX \
+    --enable-all-tests
+
 make
 make check
 make install
