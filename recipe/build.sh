ccaa5dd32b5b455886a2c25cbb810240c9539259
Andrii Osipov <aosipov@anaconda.com>
2026-04-21 00:32:12 -0700

    PKG-13772: gnupg 2.5.18

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-./configure --prefix=$PREFIX --enable-all-tests
+./configure \
+    --disable-doc \
+    --prefix=$PREFIX \
+    --with-npth-prefix=$PREFIX \
+    --with-libgpg-error-prefix=$PREFIX \
+    --with-libgcrypt-prefix=$PREFIX \
+    --with-libksba-prefix=$PREFIX \
+    --with-libassuan-prefix=$PREFIX \
+    --enable-all-tests
+
 make
 make check
 make install
